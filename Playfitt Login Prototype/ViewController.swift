fa2f305b9ce0d7ea9c85b75a6fae9a259af731c0
Ryan David Forsyth <forsyth.r@gmail.com>
2019-10-23 22:18:23 -0400

    Initial Commit

---
+++ Playfitt Login Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Playfitt Login Prototype
+//
+//  Created by Ryan David Forsyth on 2019-10-23.
+//  Copyright © 2019 Ryan F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
